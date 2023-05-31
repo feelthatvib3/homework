42698a1f5029a58f5af1fa1fd3cd816a8afa8a92
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2023-05-31 19:34:07 -0400

    Add char_conversion task

---
+++ java_basic/char_conversion/src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // first approach (ASCII table)
+        char x = '0';
+        int xx = x;
+
+        System.out.println(xx);
+        // or
+        // System.out.println((int) x);
+
+        // second approach (parseInt() method)
+        char y = '0';
+        int yy = Integer.parseInt(String.valueOf(y));
+
+        System.out.println(yy);
+
+        // third approach (getNumericValue() of the character's class method)
+        char z = '0';
+        int zz = Character.getNumericValue(z);
+
+        System.out.println(zz);
+    }
+}
