bfbc85d69a032f0fd765c71c87e9a4b68fb91e9b
feelthatvib3 <feelthatvib3@gmail.com>
2023-06-01 00:57:44 -0400

    Add char_conversion task

--- java_basic/char_conversion/src/main/java/Main.java
+++ java_basic/char_conversion/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
+=======
+
+>>>>>>> cc33f13 (Add char_conversion task)
         // first approach (ASCII table)
         char x = '0';
         int xx = x;
