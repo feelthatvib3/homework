0c6bd5fe845c38f4fd617dc061876118657d4549
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2023-06-01 18:31:01 -0400

    Add box_constructor task

---
+++ java_basic/box_constructor/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import box.Box;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(60.5, 80, 120.5);
+
+        box.calculateVolume();
+    }
+}
---
+++ java_basic/box_constructor/src/main/java/box/Box.java
@@ -0,0 +1,19 @@
+package box;
+
+public class Box {
+    public double width;
+    public double height;
+    public double length;
+
+    public Box(double width, double height, double length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public void calculateVolume() {
+        double volume = length * width * height;
+
+        System.out.println("The volume of the box is: " + (int) (volume) + " cm^3");
+    }
+}
