2072a3e4a0d0c52bca8d8a31876314bc6a509a49
feelthatvib3 <88098862+feelthatvib3@users.noreply.github.com>
2023-06-15 22:53:25 +0200

    Add alternate way

--- java_basic/months/src/main/java/Main.java
+++ java_basic/months/src/main/java/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
         System.out.print("Enter month's number (1-12): ");
         int monthNumber = sc.nextInt();
+        sc.close();
 
+        /*
         if (monthNumber == 12 || monthNumber == 1 || monthNumber == 2) {
             System.out.println("It is winter!");
         } else if (monthNumber == 3 || monthNumber == 4 || monthNumber == 5) {
@@ -18,7 +20,32 @@ public class Main {
         } else {
             System.out.println("Incorrect number!");
         }
+         */
 
-        sc.close();
+        /*
+        if (monthNumber >= 1 && monthNumber <= 12) {
+            if (monthNumber > 2 && monthNumber < 6) {
+                System.out.println("It's spring!");
+            } else if (monthNumber > 5 && monthNumber < 9) {
+                System.out.println("It's summer!");
+            } else if (monthNumber > 8 && monthNumber < 12) {
+                System.out.println("It's autumn!");
+            } else {
+                System.out.println("It's winter!");
+            }
+        } else {
+            System.out.println("Incorrect number!");
+        }
+        */
+
+        switch(monthNumber) {
+            case 1, 2, 12   -> System.out.println("It's winter!");
+            case 3, 4, 5    -> System.out.println("It's spring!");
+            case 6, 7, 8    -> System.out.println("It's summer!");
+            case 9, 10, 11  -> System.out.println("It's autumn!");
+            default         -> System.out.println("Incorrect number!");
+        }
+
+        // слишком много способов решения на одну квадратную задачу :)
     }
 }
